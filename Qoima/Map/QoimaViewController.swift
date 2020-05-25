740d8bb9fca7c6708923247d1e69b656e203ebec
Tair <adibek.t@maint.kz>
2020-05-25 12:20:51 +0600

    äs

---
+++ Qoima/Map/QoimaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QoimaViewController.swift
+//  Qoima
+//
+//  Created by root user on 5/25/20.
+//  Copyright © 2020 iitu. All rights reserved.
+//
+
+import UIKit
+
+class QoimaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
